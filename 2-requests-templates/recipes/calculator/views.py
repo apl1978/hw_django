866eb31d7789aad251bac113fb3d217a4b1488a6
apl_1978 <apl_1978@yahoo.com>
2022-08-25 17:12:08 +0300

    Added second task - Recipes and Pagination

---
+++ 2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
+
+def dish_view(request, dish):
+    template_name = 'calculator/index.html'
+    servings = int(request.GET.get('servings', 1))
+    recipe = {}
+    d_dish = DATA.get(dish)
+    if d_dish is not None:
+        for k, v in d_dish.items():
+            v = v * servings
+            recipe[k] = v
+
+    context = {
+        'recipe': recipe
+    }
+    return render(request, template_name, context)
